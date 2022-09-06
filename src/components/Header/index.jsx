3389ed8bd2dbcb38ee7e86db7fb14ebbcb75661c
Prince Codes <princecodes247@gmail.com>
2022-09-06 11:30:02 +0100

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
+import "./styles.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">Astro</div>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+      </ul>
+
+      <div className="button_set">
+        <button>A</button>
+        <button>B</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <p>Home</p>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
