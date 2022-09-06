1ed262c0c4a0b87b407445213ccdaf88cb41da73
successAgent <miesineagent@gmail.com>
2022-09-06 17:23:09 +0100

    updated

--- src/components/Card/AboutCard/AboutCard.jsx
+++ src/components/Card/AboutCard/AboutCard.jsx
@@ -1,47 +1,52 @@
-import React, { useState } from 'react'
 import AboutCardLogoOne from '../../../assets/Images/img_01-3.jpg'
 import AboutCardLogoTwo from '../../../assets/Images/img_02-3.jpg'
 import AboutCardLogoThree from '../../../assets/Images/img_03-3.jpg'
+import { Button, SmallButton } from '../../Button'
 
-function MemberCard() {
+const AboutCard = () => {
   return (
-    <>
-      <section className="member-card">
+    <section className="about-card">
+      <div>
         <div>
           <img src={AboutCardLogoOne} alt="" />
+        </div>
+        <div>
+          <SmallButton>Click</SmallButton>
           <h3>Company History</h3>
           <p>
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium totam
           </p>
         </div>
+      </div>
+      <div>
         <div>
           <img src={AboutCardLogoTwo} alt="" />
-          <h3>Company History</h3>
+        </div>
+        <div>
+          <SmallButton>Click</SmallButton>
+          <h3>Exclusive Members</h3>
           <p>
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium totam
           </p>
         </div>
+      </div>
+      <div>
         <div>
           <img src={AboutCardLogoThree} alt="" />
-          <h3>Company History</h3>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium totam
-          </p>
         </div>
         <div>
-          <img src={AboutCardLogoOne} alt="" />
-          <h3>Company History</h3>
+          <SmallButton>Click</SmallButton>
+          <h3>Working Progress</h3>
           <p>
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium totam
           </p>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
 
-export default MemberCard
+export default AboutCard
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,83 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import Header from '../../components/Header'
+import '../About/style.css'
+import aboutImageOne from '../../assets/Images/img_01.png'
+import aboutImageTwo from '../../assets/Images/reward.png'
+import aboutImageThree from '../../assets/Images/iso.png'
+import AboutCard from '../../components/Card/AboutCard/AboutCard'
+import MemberCard from '../../components/Card/MemberCard'
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <Header />
+      <div className="about-section">
+        <div className="about-section-one">
+          <h2>About</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="about-section-two">
+          <div>
+            <img src={aboutImageOne} alt="" />
+          </div>
+          <div>
+            <div>
+              <h4>About Our Company</h4>
+              <h1>Printing Your Dream Works With Printem</h1>
+            </div>
+            <p>
+              About Sed ut perspiciatis unde omnis iste natus error sit
+              voluptatem accu santium doloremque laudantium, totam rem aperiam,
+              eaque ipquae ab illo inventore veritatis et quasi architecto
+              beatae
+            </p>
+            <h5>
+              But I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system and expound the actual teachings of the
+              great explorer of the truth, the master-build human happiness. No
+              one rejects, dislikes, or avoids pleasure
+            </h5>
+            <div className="about-part-two-footer">
+              <div>
+                <img src={aboutImageTwo} alt="" />
+                <div>
+                  <h3>ISO Certified</h3>
+                  <h5>
+                    ISO Certified Denouncing pleasure and praising pain was born
+                  </h5>
+                </div>
+              </div>
+              <div>
+                <img src={aboutImageThree} alt="" />
+                <div>
+                  <h3>Award Wins</h3>
+                  <h5>Denouncing pleasure and praising pain was bor</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="about-section-three">
+          <h4>What We Offers</h4>
+          <div>
+            <h2>We Provide Lot’s Of Printing & Branding Service</h2>
+          </div>
+          <AboutCard />
+        </div>
+        <div className="about-section-four">
+          <h4>What We Offers</h4>
+          <div>
+            <h2>We Provide Lot’s Of Printing & Branding Service</h2>
+            <MemberCard/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default About;
+export default About
