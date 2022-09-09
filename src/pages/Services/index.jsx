d3cd7c3b46acf811dbd314cf3eec56b771790c1d
Prince Codes <princecodes247@gmail.com>
2022-09-09 09:02:51 +0100

    update

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
 import CountUp from "react-countup";
 import "./Services.css";
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import CounterCard from "../../components/Card/CounterCard";
+
 function Services() {
   return (
-    <div>
+    <>
+      <Header />
+      <Banner />
       <div className="pushDown"></div>
-      <CountUp end={100} />
-      <CountUp
-        start={0}
-        end={100}
-        startOnMount={false}
-        enableScrollSpy={true}
-        scrollSpyDelay={100}
-        duration={5}
-      >
-        {({ countUpRef, start }) => (
-          <div>
-            <span ref={countUpRef} />
-            <button onClick={start}>Start</button>
-          </div>
-        )}
-      </CountUp>
-    </div>
+      <div className="">
+        <CounterCard />
+        <CounterCard />
+        <CounterCard />
+      </div>
+    </>
   );
 }
 
