1c3c6769348da228ec08569e5d040b37b5e90903
Prince Codes <princecodes247@gmail.com>
2022-09-09 11:04:11 +0100

    add faq page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import { Home, About, Services } from './pages'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Home, About, Services, Faq } from "./pages";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="services" element={<Services />} />
+      <Route path="faq" element={<Faq />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
+import Faq from "./Faq";
 
-export { Home, About, Services };
+export { Home, About, Services, Faq };
