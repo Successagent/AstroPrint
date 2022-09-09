49e3cabfaaa0a68692bcaa4827dc4b3c62f8f77c
successAgent <miesineagent@gmail.com>
2022-09-09 10:56:13 +0100

    additionfaq

---
+++ src/pages/Faq/Faq.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Faq = () => {
+  return (
+    <>
+      <div className="faq-section">
+        <div className="about-section-one">
+          <h2 className="h2">About</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Faq
