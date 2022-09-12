224e05263da26bcaec30503dfeba20875fdb0797
successAgent <miesineagent@gmail.com>
2022-09-12 07:13:54 +0100

    linked Contact Page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,15 +3,43 @@ import React from 'react'
 import Banner from '../../components/Banner'
 import Header from '../../components/Header'
 
+
+import { FaMapMarkedAlt } from 'react-icons/fa'
+
+import "../Contact/Contact.css"
+import "../About/About.css"
+
 const Contact = () => {
   return (
     <>
-    <Header/>
+      {/* <Header /> */}
       <div className="Contact-section">
-        <div>
-          {/* <Banner /> */}
+        <Banner />
+        <div className="contact-section-one">
+          <div>
+            <div>
+              <FaMapMarkedAlt></FaMapMarkedAlt>
+            </div>
+            <h3 className="h3">Locations</h3>
+            <p className="h5">2055 Main Street,2nd Floor, E - Block, New York</p>
+          </div>
+          <div>
+            <div>
+              <FaMapMarkedAlt></FaMapMarkedAlt>
+            </div>
+            <h3 className="h3">Locations</h3>
+            <p className="h5">2055 Main Street,2nd Floor, E - Block, New York</p>
+          </div>
+          <div>
+            <div>
+              <FaMapMarkedAlt></FaMapMarkedAlt>
+            </div>
+            <h3 className="h3">Locations</h3>
+            <p className="h5">2055 Main Street,2nd Floor, E - Block, New York</p>
+          </div>
         </div>
       </div>
+      <Footer/>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
 import Faq from "./Faq";
+import Contact from "./Contact";
 
-export { Home, About, Services, Faq };
+export { Home, About, Services, Faq, Contact };
