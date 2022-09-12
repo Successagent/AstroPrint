e2b3d3ef4a15426f82a651ffe8fec9f3d6e89b01
successAgent <miesineagent@gmail.com>
2022-09-12 12:50:39 +0100

    animation updated

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 import Banner from '../../components/Banner'
 import Header from '../../components/Header'
-import Footer from "../../components/Footer"
-
+import Footer from '../../components/Footer'
 
 import { FaMapMarkedAlt } from 'react-icons/fa'
 
-import "../Contact/Contact.css"
-import "../About/About.css"
+import '../Contact/Contact.css'
+import '../About/About.css'
 
 const Contact = () => {
   return (
@@ -22,25 +21,51 @@ const Contact = () => {
               <FaMapMarkedAlt></FaMapMarkedAlt>
             </div>
             <h3 className="h3">Locations</h3>
-            <p className="h5">2055 Main Street,2nd Floor, E - Block, New York</p>
+            <p className="h5">
+              2055 Main Street,2nd Floor, E - Block, New York
+            </p>
           </div>
           <div>
             <div>
               <FaMapMarkedAlt></FaMapMarkedAlt>
             </div>
             <h3 className="h3">Locations</h3>
-            <p className="h5">2055 Main Street,2nd Floor, E - Block, New York</p>
+            <p className="h5">
+              2055 Main Street,2nd Floor, E - Block, New York
+            </p>
           </div>
           <div>
             <div>
               <FaMapMarkedAlt></FaMapMarkedAlt>
             </div>
             <h3 className="h3">Locations</h3>
-            <p className="h5">2055 Main Street,2nd Floor, E - Block, New York</p>
+            <p className="h5">
+              2055 Main Street,2nd Floor, E - Block, New York
+            </p>
           </div>
         </div>
+        <div className="contact-section-two">
+          <h4 className="h4">Contact Us</h4>
+          <h2 className="h2">Get In touch</h2>
+          <form className="contact-form">
+            <div>
+              <input type="text" placeholder="Write Your Name" />
+            </div>
+            <div>
+              <input type="email" placeholder="Write Your Email" />
+            </div>
+            <div>
+              <input type="phone" placeholder="Write Your Phone Number" />
+            </div>
+            <textarea placeholder="Write Your Message"></textarea>
+            <button className="faq-section-btn">Send Message</button>
+          </form>
+        </div>
+        <div className="contact-section-three">
+          
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   )
 }
